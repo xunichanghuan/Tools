da5bb32f4b3bec972b3272a7f34b136de914a09d
xunichanghuan <zhpjake@vip.qq.com>
2019-10-19 21:29:38 +0800

    Update aria2-auto-start.sh

--- aria2-auto-start.sh
+++ aria2-auto-start.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 cd ~
+mkdir -p /GoogleDrive/
 wget -O rcloned https://raw.githubusercontent.com/xunichanghuan/Tools/master/rclone-auto-start.sh
 mv rcloned /etc/init.d/
 chmod +x /etc/init.d/rcloned
 update-rc.d -f rcloned defaults
 bash /etc/init.d/rcloned start
 
+mkdir -p /Download/
 wget -N https://raw.githubusercontent.com/xunichanghuan/Tools/master/rclone-auto-upload.sh
 chmod +x rclone-auto-upload.sh
 echo "on-download-complete=/root/rclone-auto-upload.sh" >>/root/.aria2/aria2.conf
