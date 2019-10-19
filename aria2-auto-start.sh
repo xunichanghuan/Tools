52e50dc3b2531d1b17083b5c48a69bcc2bef80a0
xunichanghuan <zhpjake@vip.qq.com>
2019-10-19 21:18:22 +0800

    Update and rename aria2-auto-upload.sh to aria2-auto-start.sh

---
+++ aria2-auto-start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd ~
+wget -O rcloned https://raw.githubusercontent.com/xunichanghuan/Tools/master/rclone-auto-start.sh
+mv rcloned /etc/init.d/
+chmod +x /etc/init.d/rcloned
+update-rc.d -f rcloned defaults
+bash /etc/init.d/rcloned start
+
+wget -N https://raw.githubusercontent.com/xunichanghuan/Tools/master/rclone-auto-upload.sh
+chmod +x rclone-auto-upload.sh
+echo "on-download-complete=/root/rclone-auto-upload.sh" >>/root/.aria2/aria2.conf
+service aria2 restart
